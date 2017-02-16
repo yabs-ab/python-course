bdc7133af42e3eb67fb7b9697bc911976cb85eee
Peter Román <peterrom@gmail.com>
2017-02-16 09:02:43 +0100

    Add zipper exercise.

---
+++ zipper.py
@@ -0,0 +1,25 @@
+"""A program that zips files inside a directory individually.
+
+This program should not zip files that are already zipped!
+
+Hint: Use os.walk to find files.
+"""
+
+
+def main(path):
+    """Main entry point.
+    """
+    print(f'Zipping files in {path}...')
+    pass
+
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        print(f'Usage: {sys.argv[0]} <directory>', file=sys.stderr)
+        sys.exit(1)
+    else:
+        main()
