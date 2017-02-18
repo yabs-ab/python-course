398521c08b3867ce8dcb92c2cddc85e71a756a7f
Peter Román <peterrom@gmail.com>
2017-02-18 10:55:25 +0100

    Add solution to runeberg follow-up exercises

--- runeberg_solution.py
+++ runeberg_solution.py
@@ -1,7 +1,10 @@
 import re
 import os
 from itertools import chain
-from collections import Counter
+from itertools import tee
+from collections import defaultdict
+import random
+
 
 def strip_xml_tags(text):
     return re.sub(r'<.*?>', '', text)
@@ -26,10 +29,18 @@ def runeberg_word_gen(path):
             with open(chapter_path, 'r') as f:
                 yield from f
 
-    book_words = chain.from_iterable(book_line.split()
-                                     for book_line in book_lines())
+    without_tags = (strip_xml_tags(line) for line in book_lines())
 
-    yield from (strip_xml_tags(word) for word in book_words)
+    book_words = chain.from_iterable(book_line.split()
+                                     for book_line in without_tags)
+
+    # Here we could add more steps to the pipeline, i.e.
+
+    # casefolded = (word.casefold() for word in book_words)
+    # without_punctuation = (word.strip('.,:;-*') for word in casefolded)
+    # without_empty = (word for word in without_punctuation if word)
+
+    yield from book_words
 
 
 def word_count(path):
@@ -41,8 +52,37 @@ def word_count(path):
 def top_ten(path):
     """Top ten words used in `path`.
     """
-    counter = Counter(runeberg_word_gen(path))
-    return counter.most_common(10)
+    # There is a class in the standard library that does this:
+
+    # from collections import Counter
+    # return Counter(runeberg_word_gen(path)).most_common(10)
+
+    words = defaultdict(int)
+
+    for word in runeberg_word_gen(path):
+        words[word] += 1
+
+    return sorted(words.items(), key=lambda v: -v[1])[:10]
+
+
+def pairwise(seq):
+    a, b = tee(seq)
+    next(b)
+    return zip(a, b)
+
+
+def random_walk(path, n):
+    words = defaultdict(list)
+
+    for word, after in pairwise(runeberg_word_gen(path)):
+        words[word].append(after)
+
+    res = [random.choice(list(words.keys()))]
+
+    for _ in range(n):
+        res.append(random.choice(words[res[-1]]))
+
+    return res
 
 
 def main(path):
@@ -52,6 +92,9 @@ def main(path):
 
     for word, count in top_ten(path):
         print(word, '\t', count)
+
+    print(' '.join(random_walk(path, 200)))
+
 
 if __name__ == '__main__':
     import sys
